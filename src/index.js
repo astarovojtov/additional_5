ec38756dd6afe3ea2686ae7430488eb34d63b8be
astarovojtov <andrei.starovojtov@gmail.com>
2017-09-27 10:15:52 +0300

    Solution
    Submit solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if ( str.length == 0 || bracketsConfig.length == 0 ) 
+ 	return false;
+
+  var regexp = ''; 
+	  
+  for (let i=0; i<bracketsConfig.length; i++) {
+    for (let j=0; j<bracketsConfig[i].length; j++) {
+      
+      regexp += bracketsConfig[i][j].match(/\d/) ?
+	  bracketsConfig[i][j] : '\\' + bracketsConfig[i][j];
+      
+    }
+    
+    if (i != bracketsConfig.length-1) {
+	      regexp += '|';
+    }
+  }
+
+  regexp = new RegExp(regexp, 'g');
+  
+  while ( str.search(regexp) >= 0 ) {
+    str = str.replace(regexp,'');
+  }
+	
+  return str.length > 0 ? false : true
 }
